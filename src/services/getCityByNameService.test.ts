2251b0a34a9dc9851e5936362970b1f1565c88b1
Lucas Roseti <lc.roseti@gmail.com>
2024-04-21 13:28:35 -0300

    Mocks

---
+++ src/services/getCityByNameService.test.ts
@@ -0,0 +1,13 @@
+import { api } from './api'
+import { getCityByNameService } from './getCityByNameService'
+import { mockCityAPIResponse } from '@__tests__/mocks/api/mockCityAPIResponse'
+
+describe('Service: getCityByNameService', () => {
+  it('should return city details', async () => {
+    jest.spyOn(api, "get").mockResolvedValue({ data: mockCityAPIResponse })
+
+    const response = await getCityByNameService('São Paulo')
+
+    expect(response.length).toBeGreaterThan(0)
+  })
+})
