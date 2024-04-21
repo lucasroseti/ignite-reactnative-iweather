f998ca0d3b4a4d58957b2472016a5ea95005ee22
Lucas Roseti <lc.roseti@gmail.com>
2024-04-21 13:54:52 -0300

    Testing Hooks

---
+++ src/routes/routes.test.tsx
@@ -0,0 +1,38 @@
+import { act, screen, waitFor } from '@testing-library/react-native'
+import { saveStorageCity } from '@libs/asyncStorage/cityStorage'
+import { render } from '@__tests__/utils/customRender'
+import { mockWeatherAPIResponse } from '@__tests__/mocks/api/mockWeatherAPIResponse'
+import { api } from '@services/api'
+
+import { Routes } from '.'
+
+describe('Routes', () => {
+  it('should be render Search screen when not city selected', async () => {
+    render(
+      <Routes />
+    )
+
+    const title = await waitFor(() => screen.findByText(/^escolha um local/i))
+
+    expect(title).toBeTruthy()
+  })
+
+  it('should be render Dashboard screen when has city selected', async () => {
+    jest.spyOn(api, 'get').mockResolvedValue({ data: mockWeatherAPIResponse })
+    
+    const city = {
+      id: '1',
+      name: 'São Paulo',
+      latitude: 123,
+      longitude: 456
+    }
+
+    await saveStorageCity(city)
+
+    await act(() => waitFor(() => render(<Routes />)))
+
+    const title = screen.getByText(city.name)
+
+    expect(title).toBeTruthy()
+  })
+})
